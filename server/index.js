66b2d28e7eea85b6ac0dfd95a0f1b391bab7c9f9
ubaidgitdev-rgb <ubaidgitdev-rgb@users.noreply.github.com>
2026-01-27 10:27:04 +0530

    Remove static serving for cleaner API-only deployment on Render

--- server/index.js
+++ server/index.js
@@ -35,19 +35,10 @@ app.use('/api/blog', require('./routes/blog'));
 app.use('/api/contact', require('./routes/contact'));
 app.use('/api/admin', require('./routes/admin'));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get(/.*/, (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
-    });
-} else {
-    app.get('/', (req, res) => {
-        res.send('USWellnessCenter API is running');
-    });
-}
+// Basic health check route
+app.get('/', (req, res) => {
+    res.send('USWellnessCenter API is running');
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
